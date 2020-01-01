030563c07ed1ac57301a434c0d068e5b7964f2c1
EZLiang <47573436+EZLiang@users.noreply.github.com>
2020-01-01 11:15:19 -0500

    add more

--- site/index.php
+++ site/index.php
@@ -2,6 +2,7 @@
 <html charset="utf-8" lang="en-us">
   <head>
     <title>home|phpEditey</title>
+    <link rel="stylesheet" type="text/css" href="assets/style.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
